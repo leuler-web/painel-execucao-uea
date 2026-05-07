dc9b5d26ca6918511970207966d940a3231785ae
leuler-web <leuler@uea.edu.br>
2026-05-07 11:22:51 -0400

    Update Execucao_Orcamentaria_UEA_Drive.py

--- Execucao_Orcamentaria_UEA_Drive.py
+++ Execucao_Orcamentaria_UEA_Drive.py
@@ -74,7 +74,7 @@ st.markdown("""
     /* --- 1. CONGELAMENTO DO TOPO DO PAINEL (TÍTULO + KPIs) --- */
     .topo-congelado {
         position: sticky;
-        top: 1px;
+        top: 0px;
         background-color: white;
         z-index: 1000;
         padding-top: 10px;
